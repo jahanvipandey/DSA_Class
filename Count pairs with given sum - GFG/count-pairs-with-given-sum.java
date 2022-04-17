b4f609fe9a13331a2599f6ed1268f3fb31ecc715
jahanvipandey <78221474+jahanvipandey@users.noreply.github.com>
2022-04-17 19:18:59 +0530

    Added solution - LeetHub

---
+++ Count pairs with given sum - GFG/count-pairs-with-given-sum.java
@@ -0,0 +1,51 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+public class GFG {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            inputLine = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(inputLine[0]);
+            int k = Integer.parseInt(inputLine[1]);
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+            int ans = new Solution().getPairsCount(arr, n, k);
+            System.out.println(ans);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    int getPairsCount(int[] arr, int n, int k) {
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        int result = 0;
+        //int[] a = new int[2];
+        for(int i = 0; i < n; i++){
+            int number = k - arr[i];
+            if(map.containsKey(number)){
+                result += map.get(number);
+            }
+            if(map.containsKey(arr[i])){
+                map.put(arr[i], map.get(arr[i]) + 1);
+            } else {
+                map.put(arr[i], 1);
+            }
+                
+        }
+        return result;// code here
+    }
+}
