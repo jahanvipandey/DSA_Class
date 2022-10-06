cf2b5c1e688a95ad8a62f512202d7778675a58fd
Jahanvi Pandey <78221474+jahanvipandey@users.noreply.github.com>
2022-10-06 13:46:19 +0530

    Time: 29 ms (5.07%), Space: 45.9 MB (47.34%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.java
+++ 64-minimum-path-sum/64-minimum-path-sum.java
@@ -1,27 +1,25 @@
 class Solution {
     public int minPathSum(int[][] grid) {
+        
         int m = grid.length;
         int n = grid[0].length;
         
-        HashMap<String, Integer> memo = new HashMap<>();
-        
-        return minSum(0, 0, m, n, grid, memo);
+        return minPath(0, 0, m, n, grid, new HashMap<String, Integer>());
     }
     
-    private int minSum(int row, int col, int m, int n, int[][] grid,  HashMap<String, Integer> memo){
-        if(row >= m || col >= n)
-            return 100001;
+    private int minPath(int r, int c, int m, int n, int[][] grid, HashMap<String, Integer> memo){
+        if(r >= m || c >= n)
+            return 2001;
         
-        if(row == m-1 && col == n-1)
-            return grid[row][col];
+        if(r == m-1 && c == n-1)
+            return grid[r][c];
         
-        String key = row + "-" + col;
-        
+        String key = Integer.toString(r) + '-' + Integer.toString(c);
         if(memo.containsKey(key))
             return memo.get(key);
         
-        int right = grid[row][col] + minSum(row, col+1, m, n, grid, memo);
-        int down = grid[row][col] + minSum(row+1, col, m, n, grid, memo);
+        int right = grid[r][c] + minPath(r, c+1, m, n, grid, memo);
+        int down = grid[r][c] + minPath(r+1, c, m, n, grid, memo);
         
         memo.put(key, Math.min(right, down));
         
