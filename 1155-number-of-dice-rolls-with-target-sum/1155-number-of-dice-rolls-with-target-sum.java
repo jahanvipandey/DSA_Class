5b13e4d7f3af7845f06adbeee8b510d05c55a1f5
Jahanvi Pandey <78221474+jahanvipandey@users.noreply.github.com>
2022-10-08 16:48:34 +0530

    Time: 56 ms (26.62%), Space: 42.4 MB (56.35%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.java
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.java
@@ -1,30 +1,32 @@
 class Solution {
-    
     public int numRollsToTarget(int n, int k, int target) {
-        
-        int[][] matrix;
-        int mod = (int)Math.pow(10, 9) + 7;
-        
-        matrix = new int[n+1][target+1];
-        for (int[] row : matrix)
+        int[][] dp = new int[n+1][target+1];
+        for (int[] row : dp)
             Arrays.fill(row, -1);
-        return sum(n, k, target, matrix, mod);
+        return possibleWays(n, target, k, dp);
     }
-
-    private int sum(int n, int k, int currSum, int[][] matrix, int mod) {
-        if (n == 0 && currSum == 0) {
+    
+    private int possibleWays(int n, int target, int faces, int[][] dp){
+        
+        if(n == 0 && target == 0)
             return 1;
-        }
-        if (n == 0 || currSum <= 0)
+	
+	if(target <= 0 || n == 0)
             return 0;
+       
+        int MOD = (int)(Math.pow(10, 9)) + 7; //1e9 + 7
 
-        if (matrix[n][currSum] != -1)
-            return matrix[n][currSum];
-
-        int combos = 0;
-        for (int i=1; i<=k; i++) {
-            combos = (combos + (sum(n - 1, k, currSum - i, matrix, mod) % mod)) % mod;
+	if(dp[n][target] != -1)
+            return dp[n][target];
+        
+	int ans = 0;
+        for(int i = 1; i <= faces; i++){
+            int tempAns = possibleWays(n-1, target-i, faces, dp) % MOD;
+            ans = ans % MOD;
+            ans = (ans + tempAns) % MOD;
         }
-        return matrix[n][currSum] = combos;
+        
+        dp[n][target] = ans;
+        return dp[n][target];
     }
 }
