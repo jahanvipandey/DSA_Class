b1d1c48b69101b2cff0fc1c2ce6d30e47dbb5017
Jahanvi Pandey <78221474+jahanvipandey@users.noreply.github.com>
2022-10-08 16:44:11 +0530

    Time: 32 ms (52.60%), Space: 42.3 MB (65.95%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.java
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.java
@@ -1,14 +1,17 @@
 class Solution {
-    int[][] matrix;
-    int mod = (int)Math.pow(10, 9) + 7;
+    
     public int numRollsToTarget(int n, int k, int target) {
+        
+        int[][] matrix;
+        int mod = (int)Math.pow(10, 9) + 7;
+        
         matrix = new int[n+1][target+1];
         for (int[] row : matrix)
             Arrays.fill(row, -1);
-        return sum(n, k, target);
+        return sum(n, k, target, matrix, mod);
     }
 
-    private int sum(int n, int k, int currSum) {
+    private int sum(int n, int k, int currSum, int[][] matrix, int mod) {
         if (n == 0 && currSum == 0) {
             return 1;
         }
@@ -20,7 +23,7 @@ class Solution {
 
         int combos = 0;
         for (int i=1; i<=k; i++) {
-            combos = (combos + (sum(n - 1, k, currSum - i) % mod)) % mod;
+            combos = (combos + (sum(n - 1, k, currSum - i, matrix, mod) % mod)) % mod;
         }
         return matrix[n][currSum] = combos;
     }
